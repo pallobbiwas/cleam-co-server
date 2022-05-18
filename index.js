e274fce9e9d0d465b7ccbe061cec7dffff48e1f6
pallobbiswas <pallobbiswas326@gmail.com>
2022-05-18 09:49:27 +0600

    part2 done

--- index.js
+++ index.js
@@ -2,12 +2,50 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 
 //middletair
 
 app.use(cors());
 app.use(express.json());
+
+//database info
+/* *
+ *name: clean-car-co
+ *pass: LzDCFqWAbixY4DfO
+ */
+
+//database connection
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://clean-car-co:LzDCFqWAbixY4DfO@cluster0.95fm0.mongodb.net/?retryWrites=true&w=majority";
+
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("clean-co").collection("service");
+    console.log('db connected ');
+
+    //get api
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const result = await serviceCollection.find(query).toArray();
+      res.send(result);
+    })
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
